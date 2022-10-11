6005938f2aa49f5dfb88bdf51c950cda64e908e4
Azeem-13 <82161544+Azeem-13@users.noreply.github.com>
2022-10-11 19:46:43 +0530

    data, isFetching and error complete

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,7 +1,15 @@
 import { genres } from '../assets/constants'
-import { SongCard } from '../components';
+import { Error, Loader, SongCard } from '../components';
+import { useGetTopChartsQuery } from "../redux/services/shazamCoreApi.js";
 
 const Discover = () => {
+
+    const { data, isFetching, error } = useGetTopChartsQuery();
+
+    if (isFetching) return <Loader />
+
+    if (error) return <Error />
+
     return (
         <div className="flex flex-col">
 
@@ -19,7 +27,7 @@ const Discover = () => {
 
             {/* Song Card */}
             <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-                {[1, 2, 3, 4, 5].map((song, i) => (
+                {data?.map((song, i) => (
                     <SongCard key={song.key} song={song} i={i} />
                 ))}
             </div>
