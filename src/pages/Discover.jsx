acbe7df135cc864552a984d6b0c4a4b0299db330
Azeem-13 <82161544+Azeem-13@users.noreply.github.com>
2022-10-11 19:14:17 +0530

    SongCard logic complete

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,4 +1,5 @@
 import { genres } from '../assets/constants'
+import { SongCard } from '../components';
 
 const Discover = () => {
     return (
@@ -7,20 +8,20 @@ const Discover = () => {
             {/* Genre */}
             <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
                 <h2 className='font-bold text-3xl text-white text-left'>Discover</h2>
-                <select
-                    onChange={() => { }}
-                    value=''
-                    className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
-                >
+                <select onChange={() => { }} value='' className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'>
                     {genres.map((genre) =>
-                        <option
-                            key={genre.value}
-                            value={genre.value}
-                        >
+                        <option key={genre.value} value={genre.value}>
                             {genre.title}
                         </option>)
                     }
                 </select>
+            </div>
+
+            {/* Song Card */}
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {[1, 2, 3, 4, 5].map((song, i) => (
+                    <SongCard key={song.key} song={song} i={i} />
+                ))}
             </div>
         </div>
     )
