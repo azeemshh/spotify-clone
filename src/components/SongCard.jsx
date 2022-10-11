f62b8dc5f6ea7bc17cc34c7c7ab51b04307c2064
Azeem-13 <82161544+Azeem-13@users.noreply.github.com>
2022-10-11 20:01:42 +0530

    SongCard structure ready

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,38 @@
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import { Link } from "react-router-dom";
+import PlayPause from "./PlayPause";
+
+const SongCard = ({ song, i }) => {
+  return (
+    <div>
+
+      {/* Play and zPause functionality upon song image */}
+      <div>
+        <div>
+          <PlayPause />
+        </div>
+        <img src="" alt="" />
+      </div>
+
+
+      {/* Title and Subtitle section */}
+      <div>
+        {/* Title section */}
+        <p>
+          <Link>
+
+          </Link>
+        </p>
+
+        {/* Subtitle section */}
+        <p>
+          <Link>
+
+          </Link>
+        </p>
+      </div>
+
+    </div>
+  )
+}
 
 export default SongCard;
