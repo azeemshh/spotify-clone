648f47b346ae2e3b3571736a126dd717133fe299
Azeem-13 <82161544+Azeem-13@users.noreply.github.com>
2022-10-11 18:59:55 +0530

    genres logic complete

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,22 @@
-const Discover = () => <div>Discover</div>;
+import { genres } from '../assets/constants'
+
+const Discover = () => {
+    return (
+        <div className="">
+            <h2 className="">Discover</h2>
+            <select className="" value='' onChange={() => { }}
+            >
+                {genres.map((genre) =>
+                    <option
+                        key={genre.value}
+                        value={genre.value}
+                    >
+                        {genre.title}
+                    </option>
+                )}
+            </select>
+        </div>
+    )
+};
 
 export default Discover;
