8095cc49518bed459eaa00a4928d1839c186efe5
Azeem-13 <82161544+Azeem-13@users.noreply.github.com>
2022-10-11 20:10:12 +0530

    SongCard - song image complete

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom";
 import PlayPause from "./PlayPause";
 
 const SongCard = ({ song, i }) => {
+
+  const activeSong = 'Test'
   return (
-    <div>
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup">
 
       {/* Play and zPause functionality upon song image */}
-      <div>
-        <div>
+      <div className="relative w-full h-56 group">
+        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
           <PlayPause />
         </div>
-        <img src="" alt="" />
+        <img src={song.images?.coverart} alt="song cover" />
       </div>
 
 
